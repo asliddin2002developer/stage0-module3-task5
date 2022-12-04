16eb6fb987ab917fbb66b774268a9026dcbf8d01
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-12-05 02:03:13 +0500

    updated

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,9 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        float res = toBeDivided / 5;
+        float res = toBeDivided / 5f;
         System.out.println(res);
     }
+
+
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,7 +2,10 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
-        int seconds = (int) minutes * 60;
+        float seconds = minutes * 60;
         System.out.println(seconds);
     }
+
+
 }
+
